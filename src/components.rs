538fa646952497db629802d5a94616c3dab385bc
n3rdium <n3rdium@gmail.com>
2025-01-11 14:00:59 +0530

    Nothing

--- src/components.rs
+++
@@ -1,9 +0,0 @@
-use bevy::prelude::*;
-
-#[derive(Component)]
-pub struct Position {
-    x: f64,
-    y: f64,
-    z: f64
-}
-
--- src/planet/planet.rs
+++ src/planet/planet.rs
@@ -4,7 +4,7 @@ use bevy::prelude::*;
 pub struct Planet {}
 
 impl Planet {
-    pub fn default() -> Planet {
+    pub fn new() -> Planet {
         Planet {}
     }
 
